88a9a6e90f4139eef423bfee8c84dd0a03ecd1b1
Nishika Singh <nishikasingh0307@gmail.com>
2026-01-09 00:50:01 +0530

    Update Skills.jsx

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -5,9 +5,9 @@ const Me = ()=> {
       <section className="skills" id="skills">
         <h2><b>Skills</b></h2>
       <div className="skills-card">
-      <p><b>Technical Skills:</b> JavaScript, React.js, Node.js, Python, SQL.</p>
-      <p><b>Frameworks:</b> NumPy, Pandas, TensorFlow, Scikit-learn, Express.js, HTML, CSS.</p>
-      <p><b>Developer Tools:</b> VS Code, Git, MySQL.</p>
+      <p><b>ᯓ★ Technical Skills:</b> JavaScript, React.js, Node.js, Python, SQL.</p>
+      <p><b>ᯓ★ Frameworks:</b> NumPy, Pandas, TensorFlow, Scikit-learn, Express.js, HTML, CSS.</p>
+      <p><b>ᯓ★ Developer Tools:</b> VS Code, Git, MySQL.</p>
       </div>
     </section>
     );
