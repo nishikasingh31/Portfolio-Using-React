daff48a2969713720abd333b8c2d14d7c246c157
Nishika Singh <nishikasingh0307@gmail.com>
2026-01-09 00:40:59 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,16 +9,14 @@ const Projects = () => {
 
        <div className="project-card">
         <h3>🧠 <a href="https://github.com/nishikasingh31/Multilingual-Language-Detection-and-Translation-System">Multilingual Language Detection and Translation System</a></h3>
-        <p> ⤷ NLP pipeline to automatically detect input language and translate text using transformer-based models.</p>
-        <p> ⤷ Implemented preprocessing, language identification, and neural machine translation for multiple languages.</p>
+        <p> ⤷ A NLP pipeline to automatically detect input language and translate text using transformer-based models.</p>
         <p> ⤷ Tech Stack: Python, TensorFlow, LangID, Hugging Face Transformers </p>
       </div>
 
 
       <div className="project-card">
         <h3>🔢 <a href= "https://github.com/nishikasingh31/Handwritten-Digit-Classification-using-CNN">Handwritten Digit Classification using CNN</a></h3>
-        <p> ⤷ A deep-learning model trained on the MNIST dataset to classify handwritten digits (0–9).</p>
-        <p> ⤷ Used multiple convolutional layers, max-pooling, an additional Dropout layer to prevent overfitting, and dense layers for classification.</p>
+        <p> ⤷ A deep-learning model trained on the MNIST dataset to classify handwritten digits(0–9).</p>
         <p> ⤷ Tech Stack: Python, TensorFlow, Keras, CNN</p>
 
       </div>
